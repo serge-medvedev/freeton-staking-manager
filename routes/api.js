cfef95b9838e25348e3b4a52def2ef8564b557f7
Serge Medvedev <thismoment.main@gmail.com>
2020-06-15 19:49:48 +0300

    refactor: config parameters renamed

--- routes/api.js
+++ routes/api.js
@@ -4,14 +4,14 @@ const debug = require('debug')('api');
 const express = require('express');
 const Validator = require('../validator');
 const Datastore = require('../datastore');
-const { servers, freetonDB } = require('../config');
+const { apiServer, freetonDB } = require('../config');
 
 const router = express.Router();
 const datastore = new Datastore(freetonDB);
 
 router.get('/', async (req, res, next) => {
     try {
-        const validator = await Validator.create(servers);
+        const validator = await Validator.create([apiServer]);
         const id = await validator.run(
             await datastore.stakeSize(), await datastore.electionId(), await datastore.skipElections());
 
@@ -28,7 +28,7 @@ router.get('/', async (req, res, next) => {
 
 router.get('/config', async (req, res, next) => {
     try {
-        const validator = await Validator.create(servers);
+        const validator = await Validator.create([apiServer]);
         const result = await validator.getConfig(req.query.id);
 
         res.json(result);
@@ -42,7 +42,7 @@ router.get('/config', async (req, res, next) => {
 
 router.get('/activeElectionId', async (req, res, next) => {
     try {
-        const validator = await Validator.create(servers);
+        const validator = await Validator.create([apiServer]);
         const electorAddr = await validator.getElectorAddr();
         const result = await validator.getActiveElectionId(electorAddr);
 
