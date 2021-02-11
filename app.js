7bd8e237df3062a448b7685b2a8ac010486d4903
Serge Medvedev <thismoment.main@gmail.com>
2021-02-12 01:02:53 +0200

    feat: jwt-based auth for admin is introduced

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const debug = require('debug')('app');
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const { CronJob } = require('cron');
@@ -17,9 +18,76 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', apiRouter);
+function protectRoute(secret) {
+    const middleware = (req, res, next) => {
+        const token = req.header('FREETON-SM-APIKEY');
 
-module.exports = app;
+        if (_.isEmpty(token)) {
+            res.status(401).send('error: token is not provided');
+
+            return;
+        }
+
+        jwt.verify(token, secret, { algorithms: ["HS256"] }, (err, decoded) => {
+            if (err) {
+                debug(err);
+
+                res.status(500).send('error: token verification failed');
+
+                return;
+            }
+
+            const misfits = [
+                process.env.FREETON_SM_ADMIN_NAME !== decoded.name,
+                process.env.FREETON_SM_ADMIN_PASSWORD !== decoded.password
+            ]
+
+            if (_.some(misfits)) {
+                res.status(401).send();
+
+                return;
+            }
+
+            next();
+        });
+    }
+
+    middleware.unless = require('express-unless');
+
+    return middleware;
+}
+
+const secret = process.env.FREETON_SM_AUTH_SECRET;
+
+if (! _.isEmpty(secret)) {
+    app.use(protectRoute(secret).unless({ path: ['/auth', /\/stats*/] }));
+
+    apiRouter.post('/auth', (req, res) => {
+        const misfits = [
+            _.isEmpty(process.env.FREETON_SM_ADMIN_NAME),
+            _.isEmpty(process.env.FREETON_SM_ADMIN_PASSWORD),
+            _.isEmpty(req.body.name),
+            _.isEmpty(req.body.password),
+            process.env.FREETON_SM_ADMIN_NAME !== req.body.name,
+            process.env.FREETON_SM_ADMIN_PASSWORD !== req.body.password
+        ];
+
+        if (_.some(misfits)) {
+            res.status(401).send('error: login/password ain\'t set/provided/valid');
+
+            return;
+        }
+
+        const token = jwt.sign(
+            _.pick(req.body, ['name', 'password']),
+            secret,
+            { algorithm: 'HS256', noTimestamp: true });
+
+        res.send(token);
+    });
+}
+
+app.use('/', apiRouter);
 
 function createJobFn(fnName) {
     return async () => {
@@ -57,3 +125,5 @@ function runJobs() {
 if (periodicJobs.enabled) {
     runJobs();
 }
+
+module.exports = app;
